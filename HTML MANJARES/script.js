50bff94116769368cacc99851ec4a2e337a0672b
Guillermo Belloni <guillermo.belloni@gmail.com>
2023-05-05 19:58:49 -0300

    grid en el header y medidas para celular y tablet

---
+++ HTML MANJARES/script.js
@@ -0,0 +1,19 @@
+let header = `
+    <div class="center">
+        <img src="IMG/Kitchen-Chef-Vector-PNG-Clipart3.png" alt="" class="size-logo"/>
+      </div>
+      <div>
+        <h1 class="titulo">Confitería Manjares</h1>
+      </div>
+      <div class="center">
+        <nav class="header-menubar">
+          <ul>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="productos.html">Productos</a></li>
+            <li><a href="nuestraHistoria.html">Nuestra Historia</a></li>
+            <li><a href="contactos.html"> Contactos</a></li>
+          </ul>
+        </nav>
+      </div>
+`
+document.getElementById("idheader").innerHTML = header
