6bdef8328681f64a2d4337c6ceb52c707155d937
Guillermo Belloni <guillermo.belloni@gmail.com>
2023-05-17 17:12:15 -0300

    carrusel automatico y modificacion de footer

--- HTML MANJARES/script.js
+++ HTML MANJARES/script.js
@@ -1,19 +1,13 @@
-let header = `
-    <div class="center">
-        <img src="IMG/Kitchen-Chef-Vector-PNG-Clipart3.png" alt="" class="size-logo"/>
-      </div>
-      <div>
-        <h1 class="titulo">Confitería Manjares</h1>
-      </div>
-      <div class="center">
-        <nav class="header-menubar">
-          <ul>
-            <li><a href="index.html">Inicio</a></li>
-            <li><a href="productos.html">Productos</a></li>
-            <li><a href="nuestraHistoria.html">Nuestra Historia</a></li>
-            <li><a href="contactos.html"> Contactos</a></li>
-          </ul>
-        </nav>
-      </div>
-`
-document.getElementById("idheader").innerHTML = header
+document.addEventListener('DOMContentLoaded', function() {
+  const carruseles = document.querySelectorAll('.carrusel-fotos');
+
+  carruseles.forEach(function(carruselFotos) {
+    function moverCarrusel() {
+      const primerElemento = carruselFotos.firstElementChild;
+      carruselFotos.appendChild(primerElemento);
+    }
+
+    setInterval(moverCarrusel, 2000);
+  });
+});
+
